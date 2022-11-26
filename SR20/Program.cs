cea7e5333b2d96be5e3382cd42fcb620c3b77340
Юлия Волкова <volktul@gmail.com>
2022-11-26 14:14:03 +0300

    Добавьте файлы проекта.

---
+++ SR20/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR20
+{
+    internal class Program
+    {
+        delegate double MyDelegate(double r);
+        static void Main(string[] args)
+        {
+
+            MyDelegate myDelegate = CircleLenght;
+            double r = myDelegate(2);
+            Console.WriteLine(r);
+
+            myDelegate = CircleSquare;
+            r = myDelegate(2);
+            Console.WriteLine(r);
+
+            myDelegate = SphereVolume;
+            r = myDelegate(2);
+            Console.WriteLine(r);
+
+            Console.ReadKey();
+        }
+        static double CircleLenght(double r) => 2*Math.PI*r;
+        static double CircleSquare(double r) =>  Math.PI * r*r;
+        static double SphereVolume(double r) =>  4.0/3*Math.PI * r*r*r;
+    }
+}
